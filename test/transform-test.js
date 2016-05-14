74aa6b57103383c72c1549e75f93fd59badc0dd9
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-14 08:22:55 -0700

    functions in place that read bitmap, get the palette, invert the colors, and now working on writing to a new bitmap file

---
+++ test/transform-test.js
@@ -0,0 +1,2 @@
+// header should be the same between data and the beginning of written buffer array
+// function should alter every number but the 4th in series
--- transform.js
+++ transform.js
@@ -6,27 +6,38 @@ const ee = new (require('events'));
 
 const bitmap = {};
 
-// Inverts RGB colors (not including alpha)
-function invertColors(palette, callback, data) {
+// Inverts RGB colors (excluding alpha)
+function invertColors(palette, cb, data) {
   for (var i = 0; i < palette.length; i++){
     if (!(i % 4 === 3)) palette[i] = 256 - palette[i];
   }
-  callback(bitmap.colorPaletteRaw, data);
+  // console.log('palette after inversion', palette)
+  cb(bitmap.colorPaletteRaw, data);
 }
 
+// Constructs and writes new bitmap
 function writeNewBitmap(palette, data) {
-  palette.join('');
-  let buf = new Buffer(palette, "hex");
-  console.log('palette prior to buffer write', palette);
-  fs.writeFile(__dirname + '/palette-bitmap.bmp', buf, (err) => {
+  let paletteString = palette.join('');
+  // console.log('palette string before converting to buffer', paletteString);
+  // let buf = new Buffer(paletteString)
+  fs.writeFile(__dirname + '/palette-bitmap-new.bmp', buf, (err) => {
     if (err) console.log(err);
+    let buf = '';
+    let bufHeader = data.slice(0, 54);
+    // let
+    console.log('buf header', bufHeader)
+    let buf = new Buffer(data.toString('ascii', 0, 54))
+    console.log('headers prior to buffer write', buf);
+    buf.write(buf.toString('ascii', 0, 54));
+    // console.log('buf', buf);
   })
 }
 
-function readBitmap(callback) {
+// Reads bitmap and converts palette to an array of bytes
+function readBitmap(cb) {
   fs.readFile(__dirname + '/palette-bitmap.bmp', (err, data) => {
     if (err) console.log(err);
-    console.log('data', data);
+    console.log('headers', data.slice(0, 54));
     bitmap.type = data.toString('ascii', 0, 2);
     bitmap.size = data.readUInt32LE(2);
     bitmap.start = data.readUInt32LE(10);
@@ -37,12 +48,12 @@ function readBitmap(callback) {
     data.toString('hex', 54, 1078).match(/.{1,2}/g).map((hex) => {
       return parseInt(hex, 16);
     });
-    callback(bitmap.colorPaletteRaw, writeNewBitmap, data);
+    // console.log(bitmap);
+    cb(bitmap.colorPaletteRaw, writeNewBitmap, data);
   })
 }
 
 readBitmap(invertColors);
-// writeNewBitmap()
 
 debugger;
 
